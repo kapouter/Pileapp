6817c94b7ffa6264aeed039b098a3ba90bd135e6
kapouter <chevauxenforce@yahoo.fr>
2019-03-28 17:22:54 +0100

    feat(): Add plant detail lifespan

--- app/src/main/java/com/kapouter/pileapp/PlantDetailFragment.kt
+++ app/src/main/java/com/kapouter/pileapp/PlantDetailFragment.kt
@@ -1,15 +1,18 @@
 package com.kapouter.pileapp
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.kapouter.pileapp.adapters.PlantImageAdapter
 import com.kapouter.pileapp.model.GrovePlant
+import com.kapouter.pileapp.model.Lifespan
 import com.kapouter.pileapp.viewmodels.PlantDetailViewModel
 import kotlinx.android.synthetic.main.fragment_plant_detail.*
 import kotlinx.android.synthetic.main.fragment_plant_detail.view.*
@@ -42,5 +45,23 @@ class PlantDetailFragment : Fragment() {
         imageAdapter.images = plant.images ?: listOf()
         name.text = plant.name ?: plant.scientificName
         scientificName.text = plant.scientificName
+
+        plant.mainSpecies?.specifications?.lifespan?.apply { setLifespan(this) }
+    }
+
+    private fun setLifespan(lifespan: Lifespan) {
+        when (lifespan) {
+            Lifespan.SHORT ->
+                lifespan1.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+            Lifespan.MODERATE -> {
+                lifespan1.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+                lifespan2.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+            }
+            Lifespan.LONG -> {
+                lifespan1.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+                lifespan2.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+                lifespan3.setColorFilter(ContextCompat.getColor(context!!, R.color.green), PorterDuff.Mode.SRC_IN)
+            }
+        }
     }
 }
