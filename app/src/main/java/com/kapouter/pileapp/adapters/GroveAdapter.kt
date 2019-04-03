627b40d38abe9a77a9b04edfd283fc9a15597abf
kapouter <chevauxenforce@yahoo.fr>
2019-04-03 16:28:35 +0200

    refactor(): Refactor GroveAdapter using data binding

--- app/src/main/java/com/kapouter/pileapp/adapters/GroveAdapter.kt
+++ app/src/main/java/com/kapouter/pileapp/adapters/GroveAdapter.kt
@@ -1,8 +1,8 @@
 package com.kapouter.pileapp.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -10,39 +10,45 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.kapouter.pileapp.GroveFragmentDirections
 import com.kapouter.pileapp.R
+import com.kapouter.pileapp.databinding.ItemGroveBinding
 import com.kapouter.pileapp.model.GrovePlant
 import com.kapouter.pileapp.model.findFirstNonSvgUrl
-import kotlinx.android.synthetic.main.item_grove.view.*
 
 class GroveAdapter :
     PagedListAdapter<GrovePlant, GroveAdapter.ViewHolder>(GrovePlantDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_grove, parent, false))
+        ViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_grove, parent, false
+            )
+        )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
     }
 
-    class ViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
+    class ViewHolder(private val binding: ItemGroveBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: GrovePlant?) {
-            v.apply {
-                name.text = item?.name ?: item?.scientificName
-                scientificName.text = item?.scientificName
+            with(binding) {
+                binding.plant = item
 
-                Glide.with(v)
+                Glide.with(root)
                     .load(item?.images?.findFirstNonSvgUrl())
                     .centerCrop()
                     .placeholder(R.drawable.ic_flower)
                     .into(image)
 
-                v.setOnClickListener {
+                root.setOnClickListener {
                     if (item?.id != null)
-                        v.findNavController().navigate(
+                        root.findNavController().navigate(
                             GroveFragmentDirections.actionPlantListFragmentToPlantDetailFragment(item.id)
                         )
                 }
+
+                binding.executePendingBindings()
             }
         }
     }
